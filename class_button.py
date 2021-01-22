13b994908f0473c28b12b131d200246e1f7c202b
silverchair <carlosdanielh@mail.com>
2021-01-21 21:08:03 -0400

    add some functions

--- class_button.py
+++ class_button.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from class_label import CustomLabel
 
 BG_COLOR_BUTTON = '#3C63B8'
 FG_COLOR_BUTTON = 'white'
@@ -8,14 +8,32 @@ FONT_BUTTON = ('Open Sans', 9, 'bold')
 
 class Buttons(tk.Button):
     def __init__(self, master):
-        super().__init__()
+        super().__init__(master)
         self.master = master
 
-    def randon_button(self, texto):
+    def create_large_button(self, texto, function):
         self.button = tk.Button(self.master,
                                 width=25,
                                 text=texto,
                                 font=FONT_BUTTON,
                                 bg=BG_COLOR_BUTTON,
-                                fg=FG_COLOR_BUTTON)
+                                fg=FG_COLOR_BUTTON,
+                                command=function)
         self.button.pack(pady=5)
+
+    def create_medium_button(self,text,function):
+        self.button = tk.Button(self.master,
+                                width=6,
+                                font=FONT_BUTTON,
+                                bg=BG_COLOR_BUTTON,
+                                fg=FG_COLOR_BUTTON,
+                                command=function,
+                                text=text)
+        self.button.grid(column=2, row=1, sticky=tk.W)
+
+    def disable_button(self):
+        self.button.configure(state=tk.DISABLED)
+
+    def enable_button(self):
+        self.button.configure(state=tk.NORMAL)
+
